5855ce729615671371572030258fbc3e65e017fa
Brian Kyalo <brianmaema28@gmail.com>
2023-10-06 09:25:11 -0700

    prints all single digit numbers of base 10 prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include<stdlib.h>
+/**
+ * main - Where the Code Execution Begins
+ * Description of the Code: Prints all single digit nmumbers of base 10 starting from  0, folowed byu a new line
+ *
+ * Return 0;
+ * j variable used
+ */
+int main (void)
+{
+	char j= 'z';
+	while (j >= 'a')
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
