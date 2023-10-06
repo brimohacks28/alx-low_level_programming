c69f3bf695b2e5fc85bf2be4e96f5804ad7f7b0b
Brian Kyalo <brianmaema28@gmail.com>
2023-10-06 11:38:51 -0700

    a program that prints all the numbers of base 16 in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+nclude <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *Description: prints all single digit numbers of base 10
+ *starting from 0, followed by a new line.
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+		char c;
+		for (c ='0'; c <='9'; c++)
+		{
+			putchar(c);
+		}
+		for (c= 'a'; c <= 'f' ;c++)
+		{
+			putchar(c);
+		}
+		putchar('\n');
+		return (0);
+}
+
+
