80e29be56c2cc1594a491da65fdba864df29cb23
Brian Kyalo <brianmaema28@gmail.com>
2023-10-11 14:47:26 -0700

    Adding 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add -returns the sum of its parameters.
+ * @e: int type number 1
+ * @f: int type number 2
+ * Return: 0 is a sucess.
+ */
+int add(int e, int f)
+{
+	return (e + f);
+}
