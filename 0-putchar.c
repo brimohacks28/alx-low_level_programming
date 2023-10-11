bb43261cad308c2bd4e3b6d8dd4da41c319ca3ea
Brian Kyalo <brianmaema28@gmail.com>
2023-10-11 03:58:06 -0700

    a program that prints _putchar, followed by a new line.
    The program should return 0

---
+++ 0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Code Execution start
+ * Return: 0 Shows sucess of the Program.
+ */
+
+int main(void)
+{
+	putchar("_putchar ");
+	putchar('\n')
+	return (0);
+}
+
+
--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,1 @@
-	#include <stdio.h>
-	
-	/**
-	 * main - Program Execution Begins
-	 * return  0 : Program Returns a 0 
-	 * Sucess is reached
-	 */
-	int main(void)
-	{
-		_putchar( " putchar \n");
-		return (0);
-	}
-
+#include "main.h"
