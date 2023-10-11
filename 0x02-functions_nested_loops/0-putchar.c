af11743eeb4e7533a429255255e0527f36e0e331
Brian Kyalo <brianmaema28@gmail.com>
2023-10-11 04:08:58 -0700

    Updated on Betty Convection

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,13 @@
-#include "main.h"
+#include "main.h:"
+
+/**
+ * main - Code Execution Begins
+ * Return: 0 shows sucess.
+ */
+
+int main(void)
+{
+	putchar(" _putchar ");
+	putchar('\n');
+	return (0);
+}
