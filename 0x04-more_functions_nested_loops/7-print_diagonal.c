2e7a280bbddf0c05f1b0ffd82a8dcd720e4ef42e
Brian Kyalo <brianmaema28@gmail.com>
2023-10-12 04:08:08 -0700

    c Program to Print a diaonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - Prints line diagonally
+ * @n:character to be used in printing
+ * Return:void
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+				_putchar('\\');
+				_putchar('\n');
+		}
+	}
+}
+
