7ba8b69ece202ac70f74cde175cc05c27558b280
Brian Kyalo <brianmaema28@gmail.com>
2023-10-12 02:59:07 -0700

    Added a readme file  plus 2 programs.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul -multiplier
+ * @a: represents int variable
+ * @b: represents 2nd int variable
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	mul = (a * b);
+
+	return (mul);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ This repository is a continuation on loops. Iterations.
