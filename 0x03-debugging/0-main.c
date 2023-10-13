8101e5e9d055b1d44ed5460a9f2f5c350670cb67
Brian Kyalo <brianmaema28@gmail.com>
2023-10-12 22:40:38 -0700

    Updated main file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main -Test function  for positive or negative
+ * main -Test function for postive or negative
  * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	int = 0;
+	i = 0;
 	positive_or_negative(i);
 
 	return (0);
