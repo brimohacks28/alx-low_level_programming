16320b8f240fb6c56c86536d5b57a2d854606bae
Brian Kyalo <brianmaema28@gmail.com>
2023-10-12 21:02:37 -0700

    created a 1-main.c file to check program if it is a positive or negative

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - causes an infinate loop
+ * Return: 0 is sucess.
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+	int = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
