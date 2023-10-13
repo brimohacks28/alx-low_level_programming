cb1e534eb10a6cca37d9c4fc6716121f6c2e04fb
Brian Kyalo <brianmaema28@gmail.com>
2023-10-12 22:21:36 -0700

    testing code

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - code execution starts
- * Return: 0;
+ * main -Test function  for positive or negative
+ * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	i = 0;
+	int = 0;
 	positive_or_negative(i);
 
 	return (0);
