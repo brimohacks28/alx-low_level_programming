c8366aa3f6c9350622155dda0b3c0fb5c1bbe874
Brian Kyalo <brianmaema28@gmail.com>
2023-10-18 08:26:35 -0700

    a function that encodes a string using rot13.

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *rot13 - encodes a string using rot13
+ * @s: int type array pointer
+ * Return: encoded
+ */
+
+char *rot13(char *s)
+{
+	int j, jj;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		for (jj = 0; jj < 54; jj++)
+		{
+			s[j] = output[jj];
+			break;
+		}
+	}
+	return (s);
+}
