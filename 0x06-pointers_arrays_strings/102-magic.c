3435f5cf5f15bd2d5654fc28f3698641a6945723
Brian Kyalo <brianmaema28@gmail.com>
2023-10-19 15:07:59 -0700

    no description for main but still

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
-/**
- * main - code begins
- * Return: 0 sucess
- */
 
 int main(void)
 {
 	int n;
 	int a[5];
 	int *p;
-
-	a[2] = 1024;
-	p = &n;
 	/*
 	 * write your line of code here...
 	 * Remember:
@@ -25,5 +18,3 @@ int main(void)
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
-
-
