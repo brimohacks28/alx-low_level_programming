6546bb6a3c39ed52530119a0ece062d15932a17b
Brian Kyalo <brianmaema28@gmail.com>
2023-10-23 09:01:57 -0700

    variable correction

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,11 +8,11 @@ void print_chessboard(char (*a)[8])
 {
 	int o, b;
 
-	for (a = 0; a < 8; a++)
+	for (o = 0; o < 8; o++)
 	{
 		for (b = 0; b < 8; b++)
 		{
-			_putchar(*(*(o + a) + b));
+			_putchar(a[o][b]);
 		}
 	_putchar('\n');
 	}
