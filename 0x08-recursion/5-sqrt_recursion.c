3198e6879191dbc0586d579062d29323108391dd
Brian Kyalo <brianmaema28@gmail.com>
2023-10-24 22:13:10 -0700

    a function that returns the natural square root of a number.

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - find squareroot
+ * @n: integer
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+
+/**
+ * square - finds square root
+ * @n: integer to find square root
+ * @sq: square root
+ * Return: integer
+ */
+
+int square(int n, int sq)
+{
+	if (sq * sq == n)
+		return (val);
+	else if (sq * sq < n)
+		return (square(n, sq + 1));
+	else
+		return (-1);
+}
