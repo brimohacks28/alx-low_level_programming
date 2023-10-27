c21d46f8c408637bcd1da8e9d48c251262c51eed
Brian Kyalo <brianmaema28@gmail.com>
2023-10-27 04:35:51 -0700

    Betty Trial

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,23 @@
+#include "main.h"
+/**
+ * main - code Execution  begins
+ * commandline - whats my name
+ * @argc: the no of commmand line args
+ * @argv: An array of command line arguments
+ *
+ * Return: 0 is sucess
+ */
 
+int commandline(int argc, char *argv[])
+{
+	if (argc > 0 && argv[0] != NULL)
+       	{
+		printf("%s\n", argv[0]);
+	}
+	return(0);
+}
+
+int main(int argc, char *argv[])
+{
+	return commandline(argc, argv);
+}
