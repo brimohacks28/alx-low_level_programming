d78375b4a2e362ea775b0a5170aca4a6f2094867
Brian Kyalo <brianmaema28@gmail.com>
2023-10-27 04:13:07 -0700

    Finding new solutions

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,1 @@
-#include "main.h"
-#include <stdio.h>
-#define passive(y) (void)(x)
-/**
- * main - Execution of code begins
- * @argc: offset count
- * @argv: offset value
- * Return: 0 is sucess
- *
- */
 
-int main(int argc, char *argv[])
-{
-	passive(argc);
-	printf("%s\n", argv[0]);
-	return (0);
-}
