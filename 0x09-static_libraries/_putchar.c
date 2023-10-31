a4b676a4417cf41f105e1ba33517ba6ca853c925
Brian Kyalo <brianmaema28@gmail.com>
2023-10-30 23:47:08 -0700

    Created a
    _putchar file

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _putchar(char c);
+ * return: 0 sucess
+ */
+
+int main(int _putchar(char c))
+{
+	char c[9] = "_putchar";
+	int i = 0;
+	while ( i < 8)
+	{
+	_putchar(c[i]);
+	i++;
+	}
+
+	_putchar('\n');
+	return (0);
+}
