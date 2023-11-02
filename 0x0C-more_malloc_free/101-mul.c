06cfb50a3d19cb7ca56345c32fbb2e9f7b10b67c
Brian Kyalo <brianmaema28@gmail.com>
2023-11-02 14:35:14 -0700

    a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,147 +1,75 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * _atoi_digit - converts a character into a integer
- * @x: character that converts
- * Return: int is a sucess
+ * realloc- prototype
+ * new_size- new
+ * old_size - old
+ * ptr- pointer
+ * main- code Execution begins
+ *
+ * Return: new-ptr sucess
  */
-
-int _atoi_digit(char x)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int res;
+	if (ptr == NULL)
+	{
+		return (malloc(new_size));
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
 
-	if (x <= '9' && x >= '0')
-		res = x - '0';
-	return (res);
-}
-/**
- * _isNumber - Defines whether string is a number
- * @argv: pointer to a string.
- * Return: 0 is suceess
- */
-
-int _isNumber(char *argv)
-{
-	int a;
-
-	for (a = 0; argv[a]; a++)
-		if (argv[a] < 48 || argv[a] > 57)
-			return (1);
-	return (0);
-}
-/**
- * _calloc - allocate array of size *nmemb.
- * @nmemb: number of elements.
- * @size: size of the element
- * Return: pointer to array is sucess
- */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	char *tab;
-	unsigned int a;
-
-	tab = malloc(size * nmemb);
-
-	if (tab == NULL)
+	if (new_size == 0)
+	{
+		free (ptr);
 		return (NULL);
+	}
 
-	for (a = 0; a < (size * nmemb); a++)
-		tab[a] = '0';
+	void *new_ptr == malloc (new_size);
 
-	return (tab);
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	if (new_size > old_size)
+	{
+		memcpy(new_ptr, ptr, old_size);
+	}
+	else
+	{
+		memcpy(new_ptr, ptr, new_size);
+	}
+	free (ptr);
+
+	return (new_ptr);
 }
 /**
- * mul_array -multiply 2 arrays
- * @a1: first array.
- * @len1: length of array a1.
- * @a2: char.
- * @a3: array for the result.
- * @lena: length of array 3
- * Return: pointer to array is sucess
+ * main- function Execution Begins
+ * old_size- old
+ * new-size- new
+ * realloc- reallocated
+ * Return: void sucess
  */
 
-void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
+int main(void)
 {
-	int mul = 0, a, j;
+	int *arr = (int *)malloc (3 * sizeof(int));
 
-	j = lena;
-	for (a = len1 - 1; a >= 0; a--)
-	{
-		mul += (a1[a] - '0') * (a2 - '0') + (a3[j] - '0');
-		a3[j] = (mul % 10) + '0';
-		mul /= 10;
-		j--;
-	}
-		while (mul != 0)
-		{
-			mul += a3[j] - '0';
-			a3[k] = (mul % 10) + '0';
-			mul /= 10;
-			k--;
-		}
+	arr[0] = 1;
+	arr[1] = 2;
+	arr[2] = 3;
 
-	return (a3);
-}
-/**
- * print_array - print all array digits
- * @nb: number of elements to print
- * @a: array of elements.
- */
+	unsigned int old_size = 3;
+	unsigned int new_size = 5;
 
-void print_array(char *a, int nb)
-{
-	int i = 0;
+	int *new_arr = (int *)_realloc(arr, old_size * sizeof(int), new_size * sizeof(int));
 
-	while (a[i] == '0' && (i + 1) < nb)
+	if (new_arr != NULL)
 	{
-		i++;
+		free(new_arr);
 	}
-	for (; i < nb; i++)
-	{
-		_putchar(a[i]);
-	}
-	_putchar('\n');
-}
-/**
- * main - prints * of 2 numbers
- * @argc: array length.
- * @argv: array
- * Return: 0.
- */
-
-int main(int argc, char *argv[])
-{
-	int i, c, len1, len2, lenres;
-	char E[6] = {'E', 'r', 'r', 'o', 'r', '\n'};
-	char *tabres;
-
-	if (argc != 3 || _isNumber(argv[1]) == 1 || _isNumber(argv[2]) == 1)
-	{
-		for (i = 0; i < 6; i++)
-		{
-			_putchar(E[i]);
-		}
-		exit(98);
-	}
-	for (len1 = 0; argv[1][len1]; len1++)
-		;
-	for (len2 = 0; argv[2][len2]; len2++)
-		;
-	lenres = len1 + len2;
-	tabres = _calloc(lenres, sizeof(int));
-	if (tabres == NULL)
-	{
-		free(tabres);
-		return (0);
-	}
-	for (i = len2 - 1, c = 0; i >= 0; i--)
-	{
-		tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 -));
-		c++;
-	}
-	print_array(tabres, lenres);
-	free(tabres);
-	exit(EXIT_SUCESS);
 	return (0);
 }
