eaecbb26e71f1bec617ffd411a872a03c3c5a11a
Brian Kyalo <brianmaema28@gmail.com>
2023-11-01 20:54:04 -0700

    Wrote a function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: allocates bytes
+ * ptr - pointer representation
+ * Return: Pointer is Sucess
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return(ptr);
+}
+
