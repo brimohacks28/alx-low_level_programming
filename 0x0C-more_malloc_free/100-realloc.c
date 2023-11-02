71bf931be64929cb3bfde858910462da88ad052e
Brian Kyalo <brianmaema28@gmail.com>
2023-11-01 22:14:46 -0700

    Advanced Task : function that reallocates a memory block using malloc and free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer represent
+ * @old_size: old size
+ * @new_size: new size rep
+ * j - Represents Counter
+ *
+ * Return: pointer
+ */
+
+void void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *clone, *relloc;
+	unsigned int j;
+
+	if (ptr != NULL)
+		clone = ptr;
+
+	else
+	{
+	return (malloc(new_size));
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (0);
+	}
+	relloc = malloc(new_size);
+	if (relloc == NULL)
+		return (0);
+	for (j = 0; j < (old_size || j < new_size); j++)
+	{
+		*(relloc + j) = clone[j];
+	}
+	free(ptr);
+	return (relloc);
+}
