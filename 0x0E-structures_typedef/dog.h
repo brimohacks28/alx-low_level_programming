ecd2bd2f176da224c3c84b23c228598636cc75ff
Brian Kyalo <brianmaema28@gmail.com>
2023-11-06 13:29:39 -0800

    Modified Dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H_
+#define DOG_H_
 /**
  * struct dog - structures dog
  * @name: attribute pointer to char name's dog
@@ -13,16 +13,10 @@ struct dog
 	float age;
 	char *owner;
 };
-
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
-
 #endif
 
