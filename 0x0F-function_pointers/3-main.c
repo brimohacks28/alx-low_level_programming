f52da2d29c8474d98ff22b56c6d222f30c844ce1
Brian Kyalo <brianmaema28@gmail.com>
2023-11-08 09:10:48 -0800

    Main.c Update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,18 +10,27 @@
  * Return: 0 is sucess
  */
 
-int main(int argc, char *argv[])
+int main(int _attribute((__unused_)) argc, char *argv[])
 {
-	int k;
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
-	k = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
-	printf("%d\n", k);
+	if(get_op_func(op)==NULL || op[1]!='\0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n",get_op_func(op)(num1,num2));
 
-	return (0);
+	return(0);
 }
+
