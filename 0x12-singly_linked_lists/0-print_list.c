60cfce8e138ded199b9b7a1509c7c35f71300423
Brian Kyalo <brianmaema28@gmail.com>
2023-11-16 04:03:15 -0800

    Fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t o = 0;
+	size_t s = 0;
 
 	while (h)
 	{
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 	else
 	printf("[%u] %s\n", h->len, h->str);
 	h = h->next;
-	o++;
+	s++;
 	}
-	return (o);
+	return (s);
 }
