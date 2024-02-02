cf923ca4a6ee23aceeadee60dd5948e1792ce729
brimohacks28@gmail.com <brianmaema28@gmail.com>
2024-02-02 09:29:21 -0800

    #include hash_tables.h
    /**
     * hash_table_create - initialize and hash table
     * @size: the size of the new table
     * Return: a new table or null if somenthing went wrong
     */
    hash_table_t *hash_table_create(unsigned long int size)
    {
    hash_table_t *table = NULL;
    
    table = malloc(sizeof(hash_table_t));
    {
    return (NULL);
    }
    
    table->array = malloc(sizeof(table->array) * size);
    {
    free(table);
    return (NULL);
    }
    
    table->size = size;
    return (table);

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,32 +1,27 @@
-/*
- * File: 0-hash_table_create.c
- * Auth: Gedeon Obae Gekonge
- */
-
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
- *
- * Return: If an error occurs - NULL.
- *         else- pointer to the new hash table.
+ * hash_table_create - initialize and hash table
+ * @size: the size of the new table
+ * Return: a new table or null if somenthing went wrong
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned long int i;
+	hash_table_t *table = NULL;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+	{
 		return (NULL);
+	}
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	table->array = malloc(sizeof(table->array) * size);
+	if (!table->array)
+	{
+		free(table);
 		return (NULL);
-	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+	}
 
-	return (ht);
+	table->size = size;
+	return (table);
 }
